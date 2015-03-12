9134740245f3d04fe15acb078b6bf211a1c2abaf
geoff winner & james nielson <student@epicodus.com>
2015-03-12 10:15:59 -0700

    add a readme and test files

---
+++ test/ScrabbleTest.php
@@ -0,0 +1,15 @@
+<?php
+    require_once "src/Scrabble.php";
+
+    class ScrabbleTest extends PHPUnit_Framework_TestCase
+    {
+        function test_Scrabble()
+        {
+            //Arrange
+            $test_Scrabble = new Scrabble;
+
+            $this->assertEquals();
+        }
+    }
+
+?>
