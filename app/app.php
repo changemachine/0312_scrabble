b0e1d21cea48ee8f6bc7eeea2b3c24871e160b5c
geoff winner & james nielson <student@epicodus.com>
2015-03-12 16:47:15 -0700

    Add dictionary search function

--- app/app.php
+++ app/app.php
@@ -8,13 +8,13 @@
         array('twig.path' => __DIR__.'/../views'));
 
      $app->get("/", function() use ($app) {
-       return $app['twig']->render('home.twig');
+     return $app['twig']->render('home.twig');
 
      });
 
      $app->post("/results", function() use ($app) {
        $new_Scrabble = new Scrabble;  // INSTANCIATE YA IDGIT!
-       $score = $new_Scrabble->score($_POST['input']);//NOW USE IT!
+       $score = $new_Scrabble->score($_POST['input']);//NOW USE IT TO POST
        return $app['twig']->render('results.twig', array('score' => $score));
 
      });
--- src/Scrabble.php
+++ src/Scrabble.php
@@ -6,41 +6,64 @@
             function score($input)
           {
 
-                $score1 = array("a", "e", "i", "o", "u", "l", "n", "r", "s", "t");
-                $score2 = array("d", "g");
-                $score3 = array("b", "c", "m", "p");
-                $score4 = array("f", "h", "v", "w", "y");
-                $score5 = array("k");
-                $score8 = array("j", "x");
-                $score10 = array("q", "z");
+                $score1 = array("A", "E", "I", "O", "U", "L", "N", "R", "S", "T");
+                $score2 = array("D", "G");
+                $score3 = array("B", "C", "M", "P");
+                $score4 = array("F", "H", "V", "W", "Y");
+                $score5 = array("K");
+                $score8 = array("J", "X");
+                $score10 = array("Q", "Z");
 
-                $split_input = str_split($input);
+
+                $dictionary = file_get_contents(__DIR__."/../src/sowpods.txt");
+                $dictionary = explode("\n", $dictionary);
+                $array = array();
+                foreach($dictionary as $line){
+                  $holder = trim($line);
+                  array_push($array, $holder);
+                }
+                
+
+
+
+                $uc_input = strtoupper($input);
+                $split_input = str_split($uc_input);
                 $score = 0;
 
-                foreach($split_input as $letter){
-                    if(in_array($letter, $score1)){
-                        $score = $score+1;
+                if (in_array($uc_input, $array)){
+
+                    foreach($split_input as $letter){
+
+                        if(in_array($letter, $score1)){
+                            $score = $score+1;
+                        }
+                        elseif(in_array($letter, $score2)){
+                            $score = $score+2;
+                        }
+                        elseif(in_array($letter, $score3)){
+                            $score = $score+3;
+                        }
+                        elseif(in_array($letter, $score4)){
+                            $score = $score+4;
+                        }
+                        elseif(in_array($letter, $score5)){
+                            $score = $score+5;
+                        }
+                        elseif(in_array($letter, $score8)){
+                            $score = $score+8;
+                        }
+                        elseif(in_array($letter, $score10)){
+                            $score = $score+10;
+                        }
+
                     }
-                    elseif(in_array($letter, $score2)){
-                        $score = $score+2;
-                    }
-                    elseif(in_array($letter, $score3)){
-                        $score = $score+3;
-                    }
-                    elseif(in_array($letter, $score4)){
-                        $score = $score+4;
-                    }
-                    elseif(in_array($letter, $score5)){
-                        $score = $score+5;
-                    }
-                    elseif(in_array($letter, $score8)){
-                        $score = $score+8;
-                    }
-                    elseif(in_array($letter, $score10)){
-                        $score = $score+10;
-                    }
-                }
-                return $score;
+
+
+                } else {
+
+                $score = "That was not a real word";
+              }
+              return $score;
 
             }
         }
